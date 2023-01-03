2210ce4e0462255189dd728812b7bdb86b5897b5
Nelson De Jesus <52361682+nelsonjdsh@users.noreply.github.com>
2023-01-03 16:10:18 -0400

    Update app.module.ts

--- Book-Store.Presentation/book-store.web-api/src/app.module.ts
+++ Book-Store.Presentation/book-store.web-api/src/app.module.ts
@@ -11,23 +11,12 @@ import { Book, Book_Page } from './books/entities';
 @Module({
   imports: [
     BooksModule,
-    // TypeOrmModule.forRoot({
-    //   type: 'mysql',
-    //   host: 'localhost',
-    //   port: 3306,
-    //   username: 'root',
-    //   password: 'secret2309',
-    //   database: 'BookStore',
-    //   entities: [__dirname + './**/**/*entity{.ts,.js}'],
-    //   autoLoadEntities: true,
-    //   synchronize: true,
-    // }),
     TypeOrmModule.forFeature([Book, Book_Page]),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'us-east.connect.psdb.cloud',
-      username: 'zcxz7x7d57c2a62rs2oq',
-      password: 'pscale_pw_dUtvcsCmUqbmsO1TiOKVFOcwtkm3WPr8nHtRL5bjbXe',
+      host: '-',
+      username: '-',
+      password: '-',
       database: 'bookstore',
       entities: [__dirname + './**/**/*entity{.ts,.js}'],
       autoLoadEntities: true,
